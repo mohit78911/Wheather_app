aaa07cf72b062ca0580e82648e4ba772fdc86b30
unknown <sutharmohit409@gmail.com>
2023-04-14 13:22:50 +0530

    all code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
-// import logo from "./logo.svg";
+import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "./App.css";
+import { TextField } from "@mui/material";
 
 function App() {
   const [data, setData] = useState({});
-  // const [newTemp, setNewTemp] = useState({});
-  const URL =
-    "https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=e307084c4507dcec68f39adf684659b9&units=metric";
+  const [search, setSearch] = useState("");
+  const URL = `https://api.weatherapi.com/v1/current.json?key=7ca2e0536cbb4412aa293215231204&q=${search}&aqi=no`;
+
   const getData = () => {
     axios
       .get(URL)
       .then((resp) => setData(resp.data))
       .catch((error) => console.log(error));
   };
-  // const newData = () => {
-  //   axios
-  //     .get(URL)
-  //     .then((response) => setNewTemp(response.data.main))
-  //     .catch((error) => console.log(error));
-  // };
+
   useEffect(() => {
     getData();
   });
+
   return (
     <>
-      <h2>Hi,<br/>This Is Wheather App</h2>
-      <div>CityName : {data.name}</div>
-      <div>Temp Of City : {data.main.temp}</div>
-      <div>Min Temp Is : {data.main.temp_min}</div>
+      <br />
+      <div className="container">
+        <h2 className="fontfamily">Hey, Weather Checking App</h2>
+        <br />
+
+        <TextField
+          fullWidth
+          label="Enter Your Location"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <br />
+        <br />
+        {data.location && <div>CityName: {data.location.name}</div>}
+        {data.current && <div>Temp Of City: {data.current.temp_c}</div>}
+        {data.current && (
+          <div>
+            Temp Of City: {data.current.temp_c}
+            Icon:
+            <img src={data.current.condition.icon} alt="Weather Icon" />
+          </div>
+        )}
+      </div>
     </>
   );
 }
