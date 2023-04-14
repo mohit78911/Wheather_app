7e1575eb679c3edd93a7cd49f978685ba7374db5
unknown <sutharmohit409@gmail.com>
2023-04-14 15:18:05 +0530

    new

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -1,24 +1,47 @@
-import React from 'react'
+import React from "react";
 
 export default function Data(props) {
   return (
-    <div>
-      {props.data.location && (
-        <div>
-          CityName: <b>{props.data.location.name}</b>
+    <>
+      <div>
+        <div class="card">
+          <ul class="list-group list-group-flush ">
+            <li class="list-group-item ">
+              {props.data.location && (
+                <div>
+                  CityName: <b>{props.data.location.name}</b>
+                </div>
+              )}
+            </li>
+            <li class="list-group-item">
+              {props.data.current && (
+                <div>
+                  Temp Of City: <b>{props.data.current.temp_c}</b>
+                </div>
+              )}
+            </li>
+            <li class="list-group-item">
+              {props.data.current && (
+                <div>
+                  Icon:
+                  <img
+                    src={props.data.current.condition.icon}
+                    alt="Weather Icon"
+                  />
+                </div>
+              )}
+            </li>
+            <li class="list-group-item">
+              {props.data.current && (
+                <div>
+                  Atmosphere : <b>{props.data.current.condition.text}</b>
+                </div>
+              )}
+            </li>
+          </ul>
         </div>
-      )}
-      {props.data.current && (
-        <div>
-          Temp Of City: <b>{props.data.current.temp_c}</b>
-        </div>
-      )}
-      {props.data.current && (
-        <div>
-          Icon:
-          <img src={props.data.current.condition.icon} alt="Weather Icon" />
-        </div>
-      )}
-    </div>
+      </div>
+      <div></div>
+    </>
   );
 }
